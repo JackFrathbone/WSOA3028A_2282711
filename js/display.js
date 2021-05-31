3b739a5e8ca03bf0136e75715a3cf8179c9325a2
JackFrathbone <2282711@students.wits.ac.za>
2021-05-31 10:17:55 +0200

    Changed The Pathing
    Testing out a new way to do pathng with Githubs dir layout that doesnt break

--- js/display.js
+++ js/display.js
@@ -2,21 +2,21 @@ const templateHead = document.createElement('template');
 templateHead.innerHTML = `
 <header>
 <a href="../index.html">
-  <img src="../Images/Logo.jpeg" alt="Logo of JAHS" style="width:200px;height:200px;">
+  <img src={{site.baseurl}}"../Images/Logo.jpeg" alt="Logo of JAHS" style="width:200px;height:200px;">
   <a href="../index.html">
-  <img src="../Images/wordart.png" alt="Word art saying Johannesburg Amateur Historical Society"
+  <img src={{site.baseurl}}"../Images/wordart.png" alt="Word art saying Johannesburg Amateur Historical Society"
     style="width:1000px;height:200px;">
     <a href="../index.html">
-  <img src="../Images/Logo.jpeg" alt="Logo of JAHS" style="width:200px;height:200px;">
+  <img src={{site.baseurl}}"../Images/Logo.jpeg" alt="Logo of JAHS" style="width:200px;height:200px;">
   <hr>
 </header>
 
 <nav>
 <ul>
-  <li><h2><a href="../Articles.html"> Articles</a></h2></li>
-  <li><h2><a href="../Events.html"> Events</a></h2></li>
-  <li><h2><a href="../About-Us.html"> About the Society</a></h2></li>
-  <li><h2><a href="../Contact-Us.html"> Contact Us</a></h2></li>
+  <li><h2><a href={{site.baseurl}}"../Articles.html"> Articles</a></h2></li>
+  <li><h2><a href={{site.baseurl}}"../Events.html"> Events</a></h2></li>
+  <li><h2><a href={{site.baseurl}}"../About-Us.html"> About the Society</a></h2></li>
+  <li><h2><a href={{site.baseurl}}"../Contact-Us.html"> Contact Us</a></h2></li>
 </ul>
   <hr>
 </nav>
@@ -29,8 +29,8 @@ templateFoot.innerHTML = `
 <footer>
 <hr>
 <ul>
-<li><h2><a href="../Out-Of-Character.html"> Out Of Character</a></h2></li>
-<li><h2><a href="../Credits.html"> Credits</a></h2></li>
+<li><h2><a href={{site.baseurl}}"../Out-Of-Character.html"> Out Of Character</a></h2></li>
+<li><h2><a href={{site.baseurl}}"../Credits.html"> Credits</a></h2></li>
 </ul>
 </footer>
 `
