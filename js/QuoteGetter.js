7d0857c3595d9b9a724f5d715af45fcc5d1f9de7
JackFrathbone <2282711@students.wits.ac.za>
2021-07-05 15:20:25 +0200

    Added Quote API

--- js/QuoteGetter.js
+++ js/QuoteGetter.js
@@ -1,16 +1,19 @@
-// Callback to run when data is ready
-function reqListener() {
-    // Parse the JSON text to an object so we can get just one property
-    var data = JSON.parse(this.responseText);
-    // Append that value to the DOM.
-    document.querySelector("Article").innerHTML = data.body;
-}
+document.addEventListener("DOMContentLoaded", () => {
 
-// Create a new ajax requst
-var oReq = new XMLHttpRequest();
-// Fire callback on load event
-oReq.addEventListener("load", reqListener);
-// Create the connection to our API
-oReq.open("GET", "https://zenquotes.io/api/random");
-// Fire the request
-oReq.send();
+    const quote = document.querySelector("blockquote p");
+    const cite = document.querySelector("blockquote h3");
+
+    async function updateQuote() {
+        const response = await fetch("https://api.quotable.io/random");
+        const data = await response.json();
+        if (response.ok) {
+            quote.textContent = data.content;
+            cite.textContent = data.author;
+        } else {
+            quote.textContent = "Error";
+            console.log(data);
+        }
+    }
+
+    updateQuote();
+});
